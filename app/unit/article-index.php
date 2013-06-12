fdb6089721c4e224f4600d764e0582b331fda579
Craig Francis <craig@craigfrancis.co.uk>
2013-06-12 09:50:35 +0100

    Retrive articles from database

--- app/unit/article-index.php
+++ app/unit/article-index.php
@@ -10,11 +10,19 @@
 
 			$sql = 'SELECT
 						s.ref,
-						s.title
+						s.title,
+						COUNT(sa.id) AS article_total,
+						COUNT(sar.article_id) AS article_read
 					FROM
 						' . DB_PREFIX . 'source AS s
+					LEFT JOIN
+						' . DB_PREFIX . 'source_article AS sa ON sa.source_id = s.id
+					LEFT JOIN
+						' . DB_PREFIX . 'source_article_read AS sar ON sar.article_id = sa.id AND sar.user_id = "' . $db->escape(USER_ID) . '"
 					WHERE
 						s.deleted = "0000-00-00 00:00:00"
+					GROUP BY
+						s.id
 					ORDER BY
 						s.sort';
 
@@ -24,7 +32,7 @@
 						'url' => url('/articles/:source/', array('source' => $row['ref'])),
 						'ref' => $row['ref'],
 						'name' => $row['title'],
-						'count' => rand(5, 10),
+						'count' => ($row['article_total'] - $row['article_read']),
 					);
 
 			}
--- app/unit/article-list.php
+++ app/unit/article-list.php
@@ -4,22 +4,83 @@
 
 		public function setup($config = array()) {
 
-			$config = array_merge(array(
-					'source' => NULL,
-				), $config);
+			//--------------------------------------------------
+			// Config
 
-			$articles = array();
+				$config = array_merge(array(
+						'source' => NULL,
+						'read' => false,
+					), $config);
 
-			foreach (array(1, 3, 5, 10, 15) as $id) {
+			//--------------------------------------------------
+			// Source
 
-				$articles[] = array(
-						'url' => url('/articles/:source/', array('source' => $config['source'], 'id' => $id)),
-						'name' => 'Article ' . ucfirst($id),
-					);
+				$db = db_get();
 
-			}
+				$sql = 'SELECT
+							s.id
+						FROM
+							' . DB_PREFIX . 'source AS s
+						WHERE
+							s.ref = "' . $db->escape($config['source']) . '" AND
+							s.deleted = "0000-00-00 00:00:00"';
 
-			$this->set('articles', $articles);
+				if ($row = $db->fetch($sql)) {
+					$source_id = $row['id'];
+					$source_ref = $config['source'];
+				} else {
+					error_send('page-not-found');
+				}
+
+			//--------------------------------------------------
+			// Articles
+
+				$where_sql = '
+					sa.source_id = "' . $db->escape($source_id) . '"';
+
+				if ($config['read'] === true) {
+
+					$where_sql .= ' AND
+						sar.article_id IS NOT NULL';
+
+				} else if ($config['read'] === false) {
+
+					$where_sql .= ' AND
+						sar.article_id IS NULL';
+
+				}
+
+				$articles = array();
+
+				$sql = 'SELECT
+							sa.id,
+							sa.title
+						FROM
+							' . DB_PREFIX . 'source_article AS sa
+						LEFT JOIN
+							' . DB_PREFIX . 'source_article_read AS sar ON sar.article_id = sa.id AND sar.user_id = "' . $db->escape(USER_ID) . '"
+						WHERE
+							' . $where_sql . '
+						GROUP BY
+							sa.id
+						ORDER BY
+							sa.published ASC
+						LIMIT
+							50';
+
+				foreach ($db->fetch_all($sql) as $row) {
+
+					$articles[] = array(
+							'url' => url('/articles/:source/', array('source' => $source_ref, 'id' => $row['id'])),
+							'name' => $row['title'],
+						);
+
+				}
+
+			//--------------------------------------------------
+			// Variables
+
+				$this->set('articles', $articles);
 
 		}
 
