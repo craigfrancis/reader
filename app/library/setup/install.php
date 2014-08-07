f8b5f7dedccb35df32a342e657e8bea0ba0c898f
Craig Francis <craig@craigfrancis.co.uk>
2014-08-07 11:08:29 +0100

    Store article_count on sources

--- app/library/setup/install.php
+++ app/library/setup/install.php
@@ -1,6 +1,13 @@
 <?php
 
-	echo 'Server: ' . SERVER . "\n";
-	echo 'Private: ' . PRIVATE_ROOT . "\n";
+	if (defined('PRIVATE_ROOT') && defined('SERVER') && SERVER == 'live') {
+		if (is_file(PRIVATE_ROOT . '/database.txt')) {
+
+			$database_password = file_get_contents(PRIVATE_ROOT . '/database.txt');
+
+			echo 'Private: ' . $database_password . "\n";
+
+		}
+	}
 
 ?>
